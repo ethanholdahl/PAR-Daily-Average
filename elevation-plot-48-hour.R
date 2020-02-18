81945da6d309c16b14ddd7f9929246b7612c8717
Ethan Holdahl <eholdahl@uoregon.edu>
2020-02-17 20:28:37 -0800

    Converting to 48 hour window for efficiency

--- elevation-plot-48-hour.R
+++ elevation-plot-48-hour.R
@@ -1,13 +1,13 @@
 library(ggplot2)
-Latitude = -75
-Longitude = 0
+Latitude = 0
+Longitude = 120
 year = 2020
 day = 27
 t=.5
 Sunrise = 0 #initializing for global assignment
 Sunset = 0 #initializing for global assignment
 
-elevation = function(t,Latitude,Longitude,year,day){
+elevation = function(t, Latitude, Longitude, year, day){
   #Converting the year and day to Julian Day and Century. 2454466.5 is
   #12:00 AM of Jan 1 2008. Using mid day as the average 2454467 is Jan 1 2008
   #ifelse(year == 2008, JulianDay = 2454466+day, JulianDay = 2454467+day+(year-2008)*365);
@@ -28,7 +28,7 @@ elevation = function(t,Latitude,Longitude,year,day){
   SunDeclineDeg = 180/pi*(asin(sin(pi/180*(ObliqCorrDeg))*sin(pi/180*(SunAppLongDeg))));
   vary = tan(pi/180*(ObliqCorrDeg/2))*tan(pi/180*(ObliqCorrDeg/2));
   EqOfTimeMinutes = 4*180/pi*(vary*sin(2*pi/180*(GeomMeanLongSunDeg))-2*EccentEarthOrbit*sin(pi/180*(GeomMeanAnomSunDeg))+4*EccentEarthOrbit*vary*sin(pi/180*(GeomMeanAnomSunDeg))*cos(2*pi/180*(GeomMeanLongSunDeg))-0.5*vary*vary*sin(4*pi/180*(GeomMeanLongSunDeg))-1.25*EccentEarthOrbit*EccentEarthOrbit*sin(2*pi/180*(GeomMeanAnomSunDeg)));
-
+  
   ## Define maximum PAR ratio: Solar Constant*.487(%of radiation that makes up PAR)*scale(100)/Sin(90)
   MaxRatio=.487*1361*100;
   srssangle = 90;
@@ -41,28 +41,30 @@ elevation = function(t,Latitude,Longitude,year,day){
     HASunriseDeg = 180/pi*acos(HAval)
   }
   SunlightDuration = HASunriseDeg/180;
-
+  
   A = sin(Latitude*pi/180)*sin(SunDeclineDeg*pi/180);
   B = cos(Latitude*pi/180)*cos(SunDeclineDeg*pi/180);
-
+  
   SolarNoon = (720-4*Longitude-EqOfTimeMinutes+0*60)/1440;
-  assign("Sunrise", ((SolarNoon*1440-HASunriseDeg*4)/1440)%%1, envir = .GlobalEnv); #keep sunrise positive and between zero and 1
-  assign("Sunset", Sunrise+SunlightDuration, envir = .GlobalEnv); #assign SR/SS to global for use in other functions
-
+  Sunrise <<- ((SolarNoon*1440-HASunriseDeg*4)/1440)%%1; #keep sunrise positive and between zero and 1
+  Sunset <<- Sunrise+SunlightDuration; #assign SR/SS to global for use in other functions
+  
   C = EqOfTimeMinutes/4+Longitude;
-
+  
   return(90-(acos(A+B*cos((t*360+C+180)*pi/180)))*180/pi)
-    
+  
 }
 
-elevation(t,Latitude,Longitude,year,day)
+elevation(t, Latitude, Longitude, year, day)
 
-t = seq(Sunrise-1/8,Sunset+1/8,1/(8*100))
+precision = 100
+
+t = seq(0,2,1/(8*precision))
 ggplot(data = NULL, aes(x = t*60*60*24, y = elevation(t,Latitude,Longitude,year,day)))+
   coord_cartesian(ylim = c(-10,90))+
   geom_line()+
   scale_x_time()
-            
+
 ggplot(data = NULL, aes(x = 24*60*60*t, y = sin(elevation(t,Latitude,Longitude,year,day)*pi/180)))+
   coord_cartesian(ylim = c(-.1,1))+
   geom_line()+
@@ -79,37 +81,114 @@ ggplot(data = NULL, aes(x = t*24*60*60, y = elevation(t,Latitude,Longitude,year,
 ###Creating projections from each observation
 
 #Max PAR: .487*1361/sin(elevation)
+
 library(tidyverse)
 
-time = c(0,1/8,2/8,3/8,4/8,5/8,6/8,7/8)
-time = time+ceiling(Sunrise*8)/8
-observations = tibble(time, elevation(time, Latitude, Longitude, year, day))
-observations = observations %>%
-  select(time, elevation = `elevation(time, Latitude, Longitude, year, day)`)
+###Filling in observation and elevation data
 
-daylightObservations = observations %>%
-  mutate(maxPAR = .487*1361*sin(elevation*pi/180)) %>%
-  filter(time < Sunset)
+time_ele_PAR = tibble(t)
+time_ele_PAR = time_ele_PAR %>%
+  select(time = t) %>%
+  mutate(observation = 0==(time*8*precision)%%(precision),
+         elevation = elevation(t, Latitude, Longitude, year, day),
+         light = elevation>=0,
+         maxPAR = light*.487*1361*sin(elevation*pi/180),
+         percentMax = observation*light*runif(length(t), min = 0, max = 1),
+         PAR = percentMax*maxPAR)
 
-daylightObservations = daylightObservations %>%
-  mutate(PAR = runif(dim(daylightObservations)[1], min = 0, max = 1)*maxPAR)
+###Seperating observations
+observations = time_ele_PAR %>%
+  filter(observation==TRUE)
 
-daylightObservations = daylightObservations %>%
-  mutate(percentMax = PAR/maxPAR)
-  
-##add vector to indicate Sunrise and Sunset
-timeSS = c(Sunrise, Sunset)
+##add vector to indicate Sunrise and Sunset times
+elevation(Sunrise, Latitude, Longitude, day, year)
 
-ggplot(data = NULL, aes(x = 24*60*60*t, y = sin(elevation(t,Latitude,Longitude,year,day)*pi/180)))+
-  #coord_cartesian(ylim = c(-.1,1))+
-  geom_line()+
-  scale_x_time()+
-  geom_point(data = daylightObservations, aes(x = time*24*60*60, y = PAR/(.487*1361), color = percentMax))+
-  scale_color_viridis_c(option = "C", limits = c(0,1))+
-  geom_vline(xintercept = timeSS*24*60*60 , color = 8)+
+SS_time = c(Sunrise, Sunset, Sunrise+1, (Sunset+1)%%2)
+SS_time = sort(SS_time)
+SS_ele = round(elevation(SS_time, Latitude, Longitude, day, year), digits = 4)
+
+SS = tibble(time = SS_time, elevation = SS_ele) %>%
+  mutate(real = elevation == 0,
+         PAR = 0)
+
+wang_index = observations %>%
+  select(time, elevation, PAR)
+
+wang_index = SS %>%
+  select(time, elevation, PAR) %>%
+  rbind(wang_index, .)
+
+
+
+ggplot(data = NULL, aes(x = 24*60*60*t, y = sin(elevation(t,Latitude,Longitude,year,day)*pi/180))) +
+  #coord_cartesian(ylim = c(-.1,1)) +
+  geom_line() +
+  scale_x_time() +
+  geom_point(data = observations, aes(x = time*24*60*60, y = PAR/(.487*1361), color = percentMax)) +
+  scale_color_viridis_c(option = "C", limits = c(0,1)) +
+  geom_vline(xintercept = SS$time*24*60*60 , color = 8) +
+  geom_vline(xintercept = c(Sunrise, Sunset)*24*60*60, color = 2) +
   theme_minimal()
 
 ###Creating a function that will plot the Wang et al. algorithm for each point
+
+observations$PAR
+
+time_ele_PAR = time_ele_PAR %>%
+  mutate(wang_i_obs1 = floor(t*8),
+         wang_i_obs2 = ceiling(t*8))
+
+
+SS = SS %>%
+  mutate(slot1 = floor(time*8),
+         slot2 = ceiling(time*8))
+
+add_SS = function(SS, time_ele_PAR) {
+  for (i in 1:4) {
+    a = time_ele_PAR %>%
+      filter(observation == FALSE,
+             wang_i_obs1 == SS$slot1[i],
+             time > SS$time[i]) %>%
+      select(time) %>%
+      as_vector() %>%
+      '*'(800) + 1
+    time_ele_PAR$wang_i_obs1[a] = 16 + i
+    
+    b = time_ele_PAR %>%
+      filter(observation == FALSE,
+             wang_i_obs2 == SS$slot2[(5-i)],
+             time < SS$time[(5-i)]) %>%
+      select(time) %>%
+      as_vector() %>%
+      '*'(800) + 1
+    time_ele_PAR$wang_i_obs2[b] = 16 + (5 - i)
+  }
+  return(time_ele_PAR)
+}
+
+time_ele_PAR = add_SS(SS,time_ele_PAR)
+
+
+time_ele_PAR = time_ele_PAR %>%
+  mutate(wang_i_PAR1 = observations$PAR[time_ele_PAR$wang_i_obs1+1],
+         wang_i_PAR2 = observations$PAR[time_ele_PAR$wang_i_obs2+1])
+
+time_ele_PAR = time_ele_PAR %>%
+  mutate(wang_PAR = wang_i_PAR1+wang_i_PAR2)
+
+a = time_ele_PAR$time*8-floor(time_ele_PAR$time*8)
+(1-a)*time_ele_PAR$wang_i_PAR1+a*time_ele_PAR$wang_i_PAR2
+
+(wangt$overpass2-wangt$t)/(wangt$overpass2-wangt$overpass1)*(wangt$PAR1*(sin((wangt$t-Sunrise)*pi/(Sunset-Sunrise))/sin((wangt$overpass1-Sunrise)*pi/(Sunset-Sunrise))))+
+            (wangt$t-wangt$overpass1)/(wangt$overpass2-wangt$overpass1)*(wangt$PAR2*(sin((wangt$t-Sunrise)*pi/(Sunset-Sunrise))/sin((wangt$overpass2-Sunrise)*pi/(Sunset-Sunrise))))
+
+
+time_ele_PAR %>%
+  mutate(wang_i_PAR1 = observations$PAR[time_ele_PAR$wang_i_obs1]
+  mutate(wang_PAR = )
+
+a=observations$PAR[time_ele_PAR$wang_i_obs1]
+
 
 wangInstPAR = function(t, daylightObservations){
   l = dim(daylightObservations)[1]
